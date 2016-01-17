04a505d1719eeca91a7d9bba2c8618363f0d5bb9
Nicolas Fetter <nicofetter@gmail.com>
2016-01-17 14:06:55 +1300

    Initital commit

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('herokuNicoApp')
+  .directive('navbar', () => ({
+    templateUrl: 'components/navbar/navbar.html',
+    restrict: 'E',
+    controller: 'NavbarController',
+    controllerAs: 'nav'
+  }));
